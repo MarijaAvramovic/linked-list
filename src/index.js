0cc12374ec5460f35bdeda2de9a626a9af77d301
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2026-03-03 19:36:10 -0500

    add classes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import "./styles.css";
+
+class LinkedList {
+  constructor() {
+    this.headNode = null;
+    this._size = 0;
+  }
+
+  class Node {
+  constructor(value = null) {
+    this.value = value;
+    this.nextNode = null;
+  }
+}
+ 
